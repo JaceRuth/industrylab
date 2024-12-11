5f91570fafa573181c89a2c89b07f6051470b59b
jaceruth <jacemruth@gmail.com>
2024-12-11 12:12:58 -0600

    Okay this is the solution.

--- src/components/hashingService.tsx
+++ src/components/hashingService.tsx
@@ -1,9 +1,9 @@
 import crypto from "crypto-js";
 
-// todo: GENERATE SALT VALUE
+// TODO: GENERATE SALT VALUE
 export const generateSalt = (length: number = 16): string => {
     const array = new Uint8Array(length);
-    window.crypto.getRandomValues(array); // Use Web Crypto API
+    window.crypto.getRandomValues(array); 
     return Array.from(array, (byte) => byte.toString(16).padStart(2, "0")).join("");
 };
 
